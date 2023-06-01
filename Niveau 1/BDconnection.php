b21983657e70e360adc1a1632c96de0b7d49d89b
Meghan Grousset <megh.g@hotmail.fr>
2023-06-01 12:03:30 +0200

    suppression bdconnection

--- Niveau 1/BDconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-        /**
-         * Etape 1: Ouvrir une connexion avec la base de donnée.
-         */
-        // on va en avoir besoin pour la suite
-      /*$mysqli = new mysqli("localhost", "root", "root", "socialnetwork");?>*/
-        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");?> 
