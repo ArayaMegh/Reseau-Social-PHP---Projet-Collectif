8678540015a582ce430d4d76e6dd5e7dc7f0f2e5
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-01 12:04:09 +0200

    tentative feed

--- Niveau_1/feed.php
+++ Niveau_1/feed.php
@@ -38,7 +38,7 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                //echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
@@ -87,12 +87,10 @@
 
 
                 <?php
-                while ($post = $lesInformations->fetch_assoc())
-                {
-                include "articles.php";
-                }
-                ?>
-
+while ($post = $lesInformations->fetch_assoc()) {
+    include “articles.php”;
+}
+?>
 
             </main>
         </div>
