c121f0d6780dc4c2245fed9e1980c5e92d5275d2
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-05 15:01:17 +0200

    ajout nouvelle page tgsPErso

--- Niveau_1/articles.php
+++ Niveau_1/articles.php
@@ -1,18 +1,17 @@
 <article>
-                        <h3>
-                            <time><?php echo $post['created']?></time>
-                        </h3>
-                        <address><a href="wall.php?user_id=<?php echo $post["author_id"]; ?>"><?php echo $post["author_name"]?></a></address>
+    <h3>
+        <time><?php echo $post['created'];?></time>
+    </h3>
+    <address><a href="wall.php?user_id=<?php echo $post["author_id"]; ?>"><?php echo $post["author_name"];?></a></address>            
+    <div>
 
-                        
-                        <div>
-                            <p><?php echo $post["content"]?></p>
-                            <!-- <p></p> -->
-                            <!-- <p></p> -->
-                        </div>                                            
-                        <footer>
-                            <small>♥<?php echo $post["like_number"]?></small> 
-                            <a href="tags.php?id=<?php echo $post["tagId"]; ?>"><?php echo "#" .$post['taglist']?></a>
-                            <!-- <a href="">#piscitur</a>, -->
-                        </footer>
-                    </article>
+    <p><?php echo $post["content"];?></p>
+    <!-- <p></p> -->
+    <!-- <p></p> -->
+    </div>                                            
+    <footer>
+        <small>♥<?php echo $post["like_number"]?></small> 
+        <a href="tags.php?id=<?php echo $post["tagId"]; ?>"><?php echo "#" .$post['taglist'];?></a>
+        <!-- <a href="">#piscitur</a>, -->
+    </footer>
+    </article>
