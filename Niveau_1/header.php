bcc4523258881000a8fa1ff0c72f8788a4e07dea
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-06-06 14:49:27 +0200

    modifs header + menu

--- Niveau_1/header.php
+++ Niveau_1/header.php
@@ -1,35 +1,33 @@
+<div class="background">
+    <div class="mega-menu">
+        <div class="mega-menu__item">
+            <a href="news.php">MySafeNews</a>
+        </div>
+        
+        <div class="mega-menu__item">
+            <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']; ?>">MySafePlace</a>
+        </div>
+        
+        <div class="mega-menu__item">
+            <a href="feed.php?user_id=<?php echo $_SESSION['connected_id']; ?>">MySafeCommu</a>
+        </div>
+        
+        <div class="mega-menu__item">
+            <a href="tagsPerso.php?tag_id=<?php echo $_SESSION['connected_id']; ?>">MySafe#</a>
+        </div>
 
-<header>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <div class="background">
-        <nav class="mega-menu">
-            <div class="mega-menu__item">
-                <!-- <img src="PROGRESS-PRIDE-FLAG.png" alt="Logo de notre réseau social"/> -->
-                <a href="news.php">MySafeNews</a>
-            </div>
-            <div class="mega-menu__item">
-                <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']; ?>">MySafePlace</a>
-            </div>
-            <div class="mega-menu__item">
-                <a href="feed.php?user_id=<?php echo $_SESSION['connected_id']; ?>">MySafeCommu</a>
-            </div>
-            <div class="mega-menu__item">
-                <a href="tagsPerso.php?tag_id==<?php echo $_SESSION['connected_id']; ?>">MySafe#</a>
-            </div>
+        <div class="mega-menu__item mega-menu__trigger">
+            <div>Paramètres ⬇️</div>
             
-            <div class="mega-menu__item mega-menu__trigger">
-                 <div>
-                    <a href="#">Profil</a>
-                </div>
-                    <div class="mega-menu__content">
-                        <a href="settings.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Paramètres</a>
-                        <a href="followers.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Followers</a>
-                        <a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Abonnements</a>
-                         <a href="login.php?user_id=<?php echo $_SESSION['connected_id']; ?>">déconnexion</a>
-                    </div>
+            <div class="mega-menu__content">
+                <a href="wall.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Mon profil</a>
+                <a href="settings.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Paramètres</a>
+                <a href="followers.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Followers</a>
+                <a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Abonnements</a>
+                <a href="login.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Déconnexion</a>
             </div>
-            </div>
-        </nav>
+        </div>
+        
     </div>
-</header>
+</div>
+
