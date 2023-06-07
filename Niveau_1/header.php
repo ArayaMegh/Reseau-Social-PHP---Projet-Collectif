f385b483be7f24d5f1588c55a7b3257c2b8562b7
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-06-07 15:12:41 +0200

    Header modifié et responsive

--- Niveau_1/header.php
+++ Niveau_1/header.php
@@ -3,7 +3,7 @@
     <div class="background">
         <nav class="mega-menu">
             <div class="mega-menu__item">
-                <!-- <img src="PROGRESS-PRIDE-FLAG.png" alt="Logo de notre réseau social"/> -->
+                <img src="logo.png" alt="Logo de notre réseau social"/>
                 <a href="news.php">MySafeNews</a>
             </div>
 
@@ -20,17 +20,7 @@
                 </div>
             <?php endif; ?>
 
-            <div class="mega-menu__item mega-menu__trigger">
-                <div><a href="#"> Paramètres ⬇️ </a></div>
-                
-                <div class="mega-menu__content">
-                    <!-- <a href="wall.php?user_id=">Mon profil</a> -->
-                    <a href="settings.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Paramètres</a>
-                    <a href="followers.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Followers</a>
-                    <a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Abonnements</a>
-                    <a href="login.php?user_id=<?php echo $_SESSION['connected_id']; ?>">Déconnexion</a>
-                </div>
-            </div>
+
         </nav>
     </div>
 </header>
